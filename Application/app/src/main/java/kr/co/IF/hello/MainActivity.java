7387f4f1c8d35ca98270e6e951df48444f9c12f6
PCNN-K <chanzzang513@gmail.com>
2021-11-16 22:11:47 +0900

    ADD TTS Notification Function

--- Application/app/src/main/java/kr/co/IF/hello/MainActivity.java
+++ Application/app/src/main/java/kr/co/IF/hello/MainActivity.java
@@ -1,24 +1,35 @@
 package kr.co.IF.hello;
 
-import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.text.NumberFormat;
 import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
@@ -36,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView mQuantityTextView2;
     private EditText mNameEditText;
 
+    private TextToSpeech tts;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -47,6 +60,15 @@ public class MainActivity extends AppCompatActivity {
         mQuantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         mQuantityTextView2 = (TextView) findViewById(R.id.quantity_text_view2);
         mNameEditText = (EditText) findViewById(R.id.name_edit_text);
+
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener(){
+            @Override
+            public void onInit(int status){
+                if(status != android.speech.tts.TextToSpeech.ERROR){
+                    tts.setLanguage(Locale.KOREAN);
+                }
+            }
+        });
     }
 
     @Override
@@ -227,15 +249,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        String name = "Name : " + mNameEditText.getText();
-        String product = "Product : " + mProductTextView.getText();
-        String quantity = "Quantity : " + mQuantity;
-        String product2 = "Product : " + mProductTextView2.getText();
-        String quantity2 = "Quantity : " + mQuantity2;
+        String name = "사용자이름 : " + mNameEditText.getText();
+        String product = "품목 : " + mProductTextView.getText();
+        String quantity = "수량 : " + mQuantity;
+        String product2 = "품목 : " + mProductTextView2.getText();
+        String quantity2 = "수량 : " + mQuantity2;
 
         int price = PRICE_CARROT * mQuantity + PRICE_COCACOLA * mQuantity2;
 
-        String formattedPrice = "Total : "
+        String formattedPrice = "총액 : "
                 + NumberFormat.getCurrencyInstance(Locale.KOREA).format(price);
 
         String message = name + "\n"
@@ -244,10 +266,10 @@ public class MainActivity extends AppCompatActivity {
                 + product2 + "\n"
                 + quantity2 + "\n"
                 + formattedPrice + "\n"
-                + "Thank you!";
+                + "감사합니다.";
 
         displayMessage(message);
-        Toast.makeText(this.getApplicationContext(),"Order is completed.", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this.getApplicationContext(),"주문이 완료되었습니다.", Toast.LENGTH_SHORT).show();
 
         new Thread(){
             public void run(){
@@ -259,8 +281,7 @@ public class MainActivity extends AppCompatActivity {
             }
         }.start();
 
-        startService(new Intent(getApplicationContext(), GetService.class));
-        stopService(new Intent(getApplicationContext(), GetService.class));
+        getRequest();
 
     }
 
@@ -350,6 +371,109 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void getRequest(){
+        Timer timer = new Timer();
+        TimerTask task = new TimerTask(){
+            @Override
+            public void run(){
+                new Thread(){
+                    public void run(){
+                        try{
+                            String host_url = "http://146.56.166.36:7579/Mobius/app1/user1/order/la";
+                            HttpURLConnection conn;
+
+                            URL url = new URL(host_url);
+                            conn = (HttpURLConnection)url.openConnection();
+
+                            conn.setRequestMethod("GET");
+                            conn.setRequestProperty("Accept", "application/json");
+                            conn.setRequestProperty("X-M2M-RI", "1234");
+                            conn.setRequestProperty("X-M2M-Origin", "CAE1");
+
+                            conn.setDoInput(true);
+
+                            int responseCode = conn.getResponseCode();
+                            System.out.println(responseCode);
+
+                            InputStream is = conn.getInputStream();
+                            StringBuilder sb = new StringBuilder();
+                            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+                            String result;
+                            while((result = br.readLine())!=null){
+                                sb.append(result + "\n");
+                            }
+
+                            result = sb.toString();
+
+                            JSONObject jsonObj = new JSONObject(result);
+                            JSONObject m2m = jsonObj.getJSONObject("m2m:cin");
+                            JSONObject con = m2m.getJSONObject("con");
+
+                            int order = (int)con.get("order");
+                            if(order == 1){
+                                noti_order();
+                                noti_by_tts();
+                                timer.cancel();
+                            }
+
+                        }catch(IOException ie){
+                            ie.printStackTrace();
+                        }catch(Exception ee){
+                            ee.printStackTrace();
+                        }
+                    }
+                }.start();
+            }
+        };
+        timer.schedule(task, 1000, 5000);
+    }
+
+    public void noti_by_tts(){
+        String text = "주문이 완료되었습니다.";
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP){
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, null);
+        }
+        else{
+            tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+        }
+    }
+
+    public void noti_order(){
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder builder;
+
+        //Check if Android version is over than Oreo(API26)
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            String channelID = "channel_01";
+            String channelName = "MyChannel01";
+
+            NotificationChannel channel = new NotificationChannel(channelID, channelName, NotificationManager.IMPORTANCE_DEFAULT);
+
+            notificationManager.createNotificationChannel(channel);
+
+            builder = new NotificationCompat.Builder(this, channelID);
+        }
+        else{
+            builder = new NotificationCompat.Builder(this, "default");
+        }
+        //Setting notification alarm's icon
+        builder.setSmallIcon(R.drawable.ic_launcher_foreground);
+
+        //Setting alarm's title and text
+        builder.setContentTitle("Order App");
+        builder.setContentText("Your order is completed.");
+
+        //If alarm is clicked, then alarm would be destroyed.
+        builder.setAutoCancel(true);
+        //Setting alarm vibration
+        builder.setVibrate(new long[]{0, 2000, 1000, 3000});
+
+        Notification notification = builder.build();
+
+        notificationManager.notify(1, notification);
+    }
+
     public void increment(View view) {
         mQuantity++;
 
@@ -416,6 +540,16 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayMessage(String message) {
         mPriceTextView.setText(message);
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
     }
 
 }
