9e4eb78e8cfab0ad40d3ad04ed0010dc94554951
KooSeongMin <37334007+17011607@users.noreply.github.com>
2021-11-25 23:11:13 +0900

    Update Motor.ino
    버그 수정

--- Arduino/Motor.ino
+++ Arduino/Motor.ino
@@ -17,9 +17,26 @@ int Bin2 = 11;
 int pwm = 130;
 int Delay = 10;
 int Time = 0;
-char ward;
+int TURN_DELAY = 400;
+
+String stat = "stop";
+
+void get_startSignal(){
+  while(1){
+    if(Serial.available()){
+      String temp = Serial.readStringUntil("\n");
+      temp.trim();
+      //Serial.println("temp" + temp); //debug
+      if(temp.equals("Go")){
+        stat = "moving";
+        break;
+      }
+    }
+  }
+}
+
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(9600);  
   Serial.println("Start Receive");
 
   pinMode(L, INPUT);
@@ -34,66 +51,90 @@ void setup(){
   pinMode(enA, OUTPUT); //오른쪽 모터 속도제어
   pinMode(enB, OUTPUT); //왼쪽 모터 속도제어
 
-  
 }
-// L과 R은 미감지시 HIGH, 검정색 감지 시 LOW
-// LL과 RR은 감지시 HIGH, 미감지시 LOW
+
 void loop(){
-  //Serial.println("L : " + L);
-  //Serial.println("R : " + R);
-  String ward;
   unsigned long Now = millis();
-    
   
-  Stop();
   if(Now >= Time){
-    // 교차로 추가
-    
-    if(digitalRead(LL) == HIGH || digitalRead(RR) == HIGH){
+    if(stat.equals("cross")){
       Stop();
-      // delay(1000);
-      Serial.println("Cross Road Signal Waiting");
-      while(Serial.available() == 0)
-        ward = Serial.readString();
-      if(ward == "RGT"){
-        curveRight();
-        delay(400);
+      Serial.println("I'm in Cross"); //debug
+      String Direction = "CrossWaiting";
+      while(1){
+        Direction = Serial.readStringUntil('\n');
+        Direction.trim();
+        if(Direction.equals("LFT")){
+          Serial.println("CrossLoad Left turn"); //debug
+          stat = "moving";
+          Bleft();
+          delay(TURN_DELAY);
+          break;
+        }
+        else if(Direction.equals("RGT")){
+          Serial.println("CrossLoad right turn"); //debug
+          stat = "moving";
+          Bright();
+          delay(TURN_DELAY);
+          break;
+        }
+        else if(Direction.equals("STR")){
+          Serial.println("CrossLoad Straight"); //debug
+          stat = "moving";
+          forward();
+          delay(100);
+          break;
+        }
+      } 
+    }
+    if(stat.equals("moving")){
+      Serial.println("I'm in moving");
+      // 스탑 또는 교차로 판단
+      if(digitalRead(LL) == HIGH || digitalRead(RR) == HIGH){
+        if (digitalRead(LL) == HIGH && digitalRead(RR) == HIGH) {
+          Stop();
+          stat = "stop";
+          Serial.println("Stop Mark"); // 가장 왼쪽/오른쪽 센서 활성화 시 스탑으로 인식 //debug
+        }
+        else{
+          // 가장 왼쪽 또는 오른쪽 센서 활성화 시 교차로로 인식 
+          Stop();
+          Serial.println("CrossRoad"); 
+          stat = "cross";
+          delay(500);
+        }
       }
-      if(ward == "LFT"){
-        Bleft();
-        delay(400);
-      }
-      if(ward == "STR"){
+      // 라인트레이싱 코드
+      if (digitalRead(L) == LOW && digitalRead(R) == LOW) {
+        //직진
         forward();
-        delay(100);
+        Serial.println("Loop Forward"); //debug
       }
-    }
-    
-    // 라인트레이싱 코드
-    if (digitalRead(L) == LOW && digitalRead(R) == LOW) {
-      //직진
-      forward();
-    }
-    if (digitalRead(L) == LOW && digitalRead(R) == HIGH) {
-      //우회전
-      Bright();
-    }
-    if (digitalRead(L) == HIGH && digitalRead(R) == LOW) {
-      //좌회전
-      Bleft();
-    }
-    if (digitalRead(LL) == HIGH && digitalRead(RR) == HIGH) {
-      //스탑
-      Stop();
-    }
-    if(Now >= Time+Delay){
-      Time += Delay*3;
-    }
+      if (digitalRead(L) == LOW && digitalRead(R) == HIGH) {
+        //우회전
+        Bright();
+        Serial.println("Loop right turn"); //debug
+      }
+      if (digitalRead(L) == HIGH && digitalRead(R) == LOW) {
+        //좌회전
+        Bleft();
+        Serial.println("Loop left turn"); //debug
+      }
+      if(Now >= Time+Delay){
+        Time += Delay*1; // 딜레이 설정용, 계수를 맞춰서 딜레이 조절이 가능
+      }
+   }
+   
+   if(stat.equals("stop")){
+    Serial.println("I'm in stop"); //debug
+    Serial.println("Stop");
+    stat = "stop";
+    get_startSignal();
+   }
   }
 }
 
 void forward() {
-  //Serial.println("forward");
   digitalWrite(Ain1, HIGH);
   digitalWrite(Ain2, LOW);
   digitalWrite(Bin1, HIGH);
@@ -103,7 +144,6 @@ void forward() {
 }
 
 void Bright() {
-  //Serial.println("right");
   digitalWrite(Ain1, HIGH);
   digitalWrite(Ain2, LOW);
   digitalWrite(Bin1, HIGH);
@@ -113,7 +153,6 @@ void Bright() {
 
 }
 void Bleft() {
-  //Serial.println("left");
   digitalWrite(Ain1, HIGH); 
   digitalWrite(Ain2, LOW);
   digitalWrite(Bin1, HIGH);
@@ -123,32 +162,11 @@ void Bleft() {
 
 }
 void Stop() {
-  //Serial.println("Stop");
   digitalWrite(Ain1, LOW);
   digitalWrite(Ain2, LOW);
   digitalWrite(Bin1, LOW);
-  digitalWrite(Bin2, LOW);
+  digitalWrite(Bin2, LOW);   
   analogWrite(enA, 0);
   analogWrite(enB, 0);
 
-}
-
-void curveRight(){
-  //Serial.println("right");
-  digitalWrite(Ain1, HIGH);
-  digitalWrite(Ain2, LOW);
-  digitalWrite(Bin1, HIGH);
-  digitalWrite(Bin2, LOW);
-  analogWrite(enA, pwm-50);
-  analogWrite(enB, pwm);
-}
-
-void curveLeft(){
-  //Serial.println("right");
-  digitalWrite(Ain1, HIGH);
-  digitalWrite(Ain2, LOW);
-  digitalWrite(Bin1, HIGH);
-  digitalWrite(Bin2, LOW);
-  analogWrite(enA, pwm);
-  analogWrite(enB, pwm-50);
 }
