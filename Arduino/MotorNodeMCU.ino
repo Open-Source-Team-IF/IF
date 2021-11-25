17ef669afd8b4cecc5df2332a7608d1a1209e290
KooSeongMin <37334007+17011607@users.noreply.github.com>
2021-11-25 22:08:25 +0900

    Update MotorNodeMCU.ino
    서버로부터 stat를 받아 작동

--- Arduino/MotorNodeMCU.ino
+++ Arduino/MotorNodeMCU.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <Arduino.h>
+#include <SoftwareSerial.h>
 
 char* ssid = "SF_LOW";
 char* password = "t1v0r20!^";
@@ -15,6 +16,8 @@ unsigned long timerDelay = 5000;
 
 unsigned long Next_Setted_Time = 0;
 
+String stat = "waiting";
+
 String jsonParse(String str, String findstr){
   int index = str.indexOf(findstr) + findstr.length() + 3;
   int index2 = str.indexOf('"', index);
@@ -24,6 +27,7 @@ String jsonParse(String str, String findstr){
 
 void getWard(){
   String ward;
+  Serial.println("getting ward");
   if(WiFi.status() == WL_CONNECTED){
     HTTPClient http;
     http.begin(serverName + Name + "/direction/la");
@@ -34,7 +38,7 @@ void getWard(){
     if(httpResponseCode == 200){
       String payload = http.getString();
       ward = jsonParse(payload, "con");
-      Serial.println(ward); //debug용 
+      Serial.println("Q"+ward); //debug용 
     }
     else{
       Serial.println("Response Code is not 200 !!");
@@ -43,30 +47,64 @@ void getWard(){
   }   
 }
 
+int startSignal(){
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+    http.begin(serverName + Name + "/status/la");
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("X-M2M-RI","/Mobius");
+    http.addHeader("X-M2M-Origin","SOrigin");
+    int httpResponseCode = http.GET();
+    if(httpResponseCode == 200){
+      String payload = http.getString();
+      flag = jsonParse(payload, "con");
+      if(flag == "moving")
+        Serial.println("G");
+    }
+    else{
+      Serial.println("Response Code is not 200 !!");
+    }
+    http.end();
+  }   
+
+  
+}
+
 void setup() {
   Serial.begin(9600); 
 
   WiFi.begin(ssid, password);
-  Serial.println("Connecting");
+  //Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    //Serial.print(".");
   }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
- 
-  Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
-
+  //Serial.println("");
+  //Serial.print("Connected to WiFi network with IP Address: ");
+  //Serial.println(WiFi.localIP());
+  //Serial.println("Timer set to 5 seconds (timerDelay variable), it will take 5 seconds before publishing the first reading.");
 }
 
 void loop() {
-      
-      if(Serial.available()){
-        String temp = Serial.readString();
-        //Serial.println("temp : " + temp);
-        if(temp == "Cross Road Signal Waiting\n") getWard();  
+      if(stat == "moving"){
+        if(Serial.available()){
+          //Serial.println("node MCU");
+          String temp = Serial.readStringUntil('\n');
+          //Serial.println("temp : " + temp);
+          if(temp[0] == 'C'){
+            if(temp[1] == 'B'){
+              getWard();  
+            }
+          }
+          if(temp[0] == 'S'){
+            if(temp[1] == 'P'){
+              flag == "stop";
+            }
+          }
+        }
       }
-      
-      
+      else{
+        startSignal();
+        delay(1000);
+      }
 }
