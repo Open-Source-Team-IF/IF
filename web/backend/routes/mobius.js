32a750ea1b3ea8d39d4f67178ac6f04598d21380
smh0346 <smh0346@naver.com>
2021-11-27 22:36:25 +0900

    Add files via upload

--- web/backend/routes/mobius.js
+++ web/backend/routes/mobius.js
@@ -3,6 +3,16 @@ var router = express.Router();
 
 var connection = require('../db');
 
+router.get('/error', function(req, res, next) {
+  connection.query('SELECT con FROM cin WHERE pi="/Mobius/server/error" ', (error, rows, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Internal Server Error');
+    }
+    console.log(': ', rows);
+    res.send(rows);
+  });
+});
 router.get('/soldout', function(req, res, next) {
   connection.query('SELECT con from cin where pi="/Mobius/server/soldout"', (error, rows, fields) => {
     if (error) {
@@ -14,8 +24,9 @@ router.get('/soldout', function(req, res, next) {
   });
 });
 
-router.get('/error', function(req, res, next) {
-  connection.query('SELECT con FROM cin WHERE pi="/Mobius/server/error" ', (error, rows, fields) => {
+router.post('/delerr', function(req, res, next) {
+  const task = req.body.task
+  connection.query('DELETE FROM cin WHERE (pi="/Mobius/server/error" OR pi="/Mobius/server/soldout") AND con like "%'+task+'%"', (error, rows, fields) => {
     if (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
@@ -35,20 +46,9 @@ router.get('/list', function(req, res, next) {
     res.send(rows);
   });
 });
-
 router.post('/delete', function(req, res, next) {
-  connection.query('DELETE FROM cin WHERE pi="/Mobius/web/product" AND con like "%'+req.body.product+'%'+req.body.stand+'%"', (error, rows, fields) => {
-    if (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-    console.log(': ', rows);
-    res.send(rows);
-  });
-});
-
-router.post('/delerr', function(req, res, next) {
-  connection.query('DELETE FROM cin WHERE (pi="/Mobius/server/error" OR pi="/Mobius/server/soldout") AND con like "%'+req.body.task+'%"', (error, rows, fields) => {
+  const product = req.body.product, stand = req.body.stand;
+  connection.query('DELETE FROM cin WHERE pi="/Mobius/web/product" AND con like "%'+product+'%'+stand+'%"', (error, rows, fields) => {
     if (error) {
       console.error(error);
       res.status(500).send('Internal Server Error');
